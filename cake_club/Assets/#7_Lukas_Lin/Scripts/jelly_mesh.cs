d496d72a3386ee9bd019b45f8b0d63c3b8564c3b
LeaSuLinHabel <lea.habel@yahoo.de>
2021-12-05 16:52:15 +0100

    tutorial 1

--- cake_club/Assets/#7_Lukas_Lin/Scripts/jelly_mesh.cs
+++ cake_club/Assets/#7_Lukas_Lin/Scripts/jelly_mesh.cs
@@ -1,16 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+ 
 public class jelly_mesh : MonoBehaviour
 {
     public float Intensity = 1f;
     public float Mass = 1f;
-    public float stiffness = 1f;
-    public float damping = 0.75f;
-
+    public float stifness = 1f;
+    public float dampining = 0.75f;
     private Mesh OriginalMesh, MeshClone;
-    private MeshRenderer renderer;
+    private MeshRenderer mrenderer;
     private JellyVertex[] jv;
     private Vector3[] vertexArray;
     // Start is called before the first frame update
@@ -19,53 +18,51 @@ public class jelly_mesh : MonoBehaviour
         OriginalMesh = GetComponent<MeshFilter>().sharedMesh;
         MeshClone = Instantiate(OriginalMesh);
         GetComponent<MeshFilter>().sharedMesh = MeshClone;
-        renderer = GetComponent<MeshRenderer>();
+        mrenderer = GetComponent<MeshRenderer>();
+ 
         jv = new JellyVertex[MeshClone.vertices.Length];
         for (int i = 0; i < MeshClone.vertices.Length; i++)
-        {
             jv[i] = new JellyVertex(i, transform.TransformPoint(MeshClone.vertices[i]));
-        }
-
     }
-
+ 
     // Update is called once per frame
     void FixedUpdate()
     {
         vertexArray = OriginalMesh.vertices;
-        for (int i = 0; i < jv.Length; i++)
+        for(int i = 0; i < jv.Length; i++)
         {
             Vector3 target = transform.TransformPoint(vertexArray[jv[i].ID]);
-            float intensity = (1 - (renderer.bounds.max.y - target.y) / renderer.bounds.size.y) * Intensity;
-            jv[i].Shake(target, Mass, stiffness, damping);
-            target = transform.InverseTransformPoint(jv[i].Position);
-            vertexArray[jv[i].ID] = Vector3.Lerp(vertexArray[jv[i].ID], target, intensity);
-
+            float intesity = (1 - (mrenderer.bounds.max.y - target.y) / mrenderer.bounds.size.y) * Intensity;
+            jv[i].Shake(target, Mass, stifness, dampining);
+            target = transform.InverseTransformPoint(jv[i].position);
+            vertexArray[jv[i].ID] = Vector3.Lerp(vertexArray[jv[i].ID], target, intesity);
         }
         MeshClone.vertices = vertexArray;
     }
-
+ 
     public class JellyVertex
     {
         public int ID;
-        public Vector3 Position;
-        public Vector3 velocity, Force;
-
-        public JellyVertex(int _id, Vector3 _pos)
+        public Vector3 position;
+        public Vector3 velocity, force;
+ 
+        public JellyVertex(int _id,Vector3 _pos)
         {
             ID = _id;
-            Position = _pos;
+            position = _pos;
+ 
         }
-
+ 
         public void Shake(Vector3 target, float m, float s, float d)
         {
-            Force = (target - Position) * s;
-            velocity = (velocity + Force / m) * d;
-            Position += velocity;
-            if ((velocity + Force + Force / m).magnitude < 0.001f)
+            force = (target - position) * s;
+            velocity = (velocity + force / m) * d;
+            position += velocity;
+            if ((velocity + force + force / m).magnitude < 0.001f)
             {
-                Position = target;
+                position = target;
             }
+ 
         }
-
     }
 }
